20c92815c44e18444e4c9bd50f5173e8708ba366
Slipak Bohdan <fear3494@gmail.com>
2021-05-25 05:56:00 +0300

    git init

--- static/js/common.js
+++ static/js/common.js
@@ -34,3 +34,22 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+$(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $(".hero-wrapper").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * +0.5 ;
+        var newvalueY = height * pageY * +0.5;
+        var carnewvalueX = width * pageX * +0.5+10;
+        var carnewvalueY = height * pageY * +0.5+10;
+        $('.pilot-img').css("bottom", "-"+newvalueX+"%");
+        $('.pilot-img').css("left", "-"+newvalueY+"%");
+        $('.car-img').css("bottom", "-"+carnewvalueX+"%");
+        $('.car-img').css("left", carnewvalueY+"%");
+    });
+});
