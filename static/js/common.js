be9b237f23657290e06099a88d475beaaa0c3833
Slipak Bohdan <fear3494@gmail.com>
2021-05-25 08:21:06 +0300

    git init

--- static/js/common.js
+++ static/js/common.js
@@ -1,29 +1,5 @@
-// $( function() {
-//     $( ".glitch-img" ).mgGlitch({
-//         // set 'true' to stop the plugin
-//         destroy : false,
-//         // set 'false' to stop glitching
-//         glitch: true,
-//         // set 'false' to stop scaling
-//         scale: true,
-//         // set 'false' to stop glitch blending
-//         blend : true,
-//         // select blend mode type
-//         blendModeType : 'hue',
-//         // set min time for glitch 1 elem
-//         glitch1TimeMin : 200,
-//         // set max time for glitch 1 elem
-//         glitch1TimeMax : 400,
-//         // set min time for glitch 2 elem
-//         glitch2TimeMin : 10,
-//         // set max time for glitch 2 elem
-//         glitch2TimeMax : 100,
-//     });
-// });
-
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 2,
-    centeredSlides: true,
     loop: true,
     pagination: {
         el: ".swiper-pagination",
@@ -45,11 +21,14 @@ $(document).ready(function() {
         var pageY = e.pageY - ($(window).height() / 2);
         var newvalueX = width * pageX * +0.5 ;
         var newvalueY = height * pageY * +0.5;
-        var carnewvalueX = width * pageX * +0.5+10;
-        var carnewvalueY = height * pageY * +0.5+10;
-        $('.pilot-img').css("bottom", "-"+newvalueX+"%");
-        $('.pilot-img').css("left", "-"+newvalueY+"%");
-        $('.car-img').css("bottom", "-"+carnewvalueX+"%");
-        $('.car-img').css("left", carnewvalueY+"%");
+        var carnewvalueX = width * pageX * +0.2+10;
+        var carnewvalueY = height * pageY * +0.2+10;
+        if ($(window).width() >= 992) {
+            $('.pilot-img').css("bottom", "-"+newvalueX+"%");
+            $('.pilot-img').css("left", "-"+newvalueY+"%");
+            $('.car-img').css("bottom", "-"+carnewvalueX+"%");
+            $('.car-img').css("left", carnewvalueY+"%");
+        }else {}
+
     });
 });
